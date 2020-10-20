3d9fca073f170e1cf92594151f45265569118547
David Wandratsch <david.wandratsch@htl-wels.at>
2020-10-20 21:08:11 +0200

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * @author David Wandratsch, 3BHIT
+ * @date 16.10.2020
+ * @version 1.0
+ */
+
+public class Main extends Application
+{
+    public static void main(String[] args)
+    {
+        Application.launch();
+    }
+
+    @Override
+    public void start(Stage stage)
+    {
+
+    }
+}
