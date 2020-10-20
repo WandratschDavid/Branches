fcc3c22564f64ee1397d8b371b4e4d1cd0eea9bc
David Wandratsch <david.wandratsch@htl-wels.at>
2020-10-20 21:38:41 +0200

    Created Labels, TextFields and Buttons

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -20,6 +23,26 @@ public class Main extends Application
     public void start(Stage stage)
     {
         VBox box_main = new VBox();
+
+        Label gradCelsius = new Label("Grad Celsius");
+        gradCelsius.setMinHeight(40);
+        gradCelsius.setStyle("-fx-font-size:20");
+        box_main.getChildren().addAll(gradCelsius);
+
+        TextField textFieldGradCelsius = new TextField();
+        box_main.getChildren().addAll(textFieldGradCelsius);
+
+        Label gradFahrenheit = new Label("Grad Fahrenheit");
+        gradFahrenheit.setMinHeight(40);
+        gradFahrenheit.setStyle("-fx-font-size:20");
+        box_main.getChildren().addAll(gradFahrenheit);
+
+        TextField textFieldGradFahrenheit = new TextField();
+        box_main.getChildren().addAll(textFieldGradFahrenheit);
+
+        Button convertCtoF = new Button("Convert C -> F");
+        Button convertFtoC = new Button("Convert F -> C");
+        box_main.getChildren().addAll(convertCtoF, convertFtoC);
 
         Scene scene = new Scene(box_main);
         stage.setTitle("Temp. Converter");
